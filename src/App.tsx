597ae5936919b9feddd3df3c00f381c2b1875242
Przemek <p.ostrowinski@gmail.com>
2019-03-30 16:53:29 +0100

    add babelrc and gitignore to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Button } from 'antd';
 import 'antd/dist/antd.less';
-import './assets/scss/variables.scss';
 import * as React from "react";
+import './assets/scss/variables.scss';
 
 const App: React.FC<{}> = (): JSX.Element => (
     <Button type='primary'>Hello</Button>
